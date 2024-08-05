2dcef531b7203d5c50b14f3e44d7409a6675ba68
martin obodo <martinobodo99@gmail.com>
2024-08-05 21:06:29 +0000

    questions for quiz

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Questions for quiz*/
+
+const quizData = [
+    {
+      question: "what is a spade used for?", 
+      options:  ["Digging" , "Strimming", "Hedgecutting", Jetwashing" ],
+      answer: "Paris"
+    }, 
+    {
+      question: "what is a rake used for?", 
+      options:  ["Rakeing" , "Strimming", "Hedgecutting", Jetwashing" ],
+      answer: "Rakeing"
+     }, 
+        
+      ];
